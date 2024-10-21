4fd4f18a87d2f8333d1b9048f1cd0f74896057c7
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-10-21 11:41:05 -0300

    Update background.js

--- Chrome-extensão/background.js
+++ Chrome-extensão/background.js
@@ -1,18 +1,33 @@
-// Função para coletar todos os cookies de todos os sites
-function getAllCookies() {
-    chrome.cookies.getAll({}, function(cookies) {
-        console.log("Total de cookies coletados: " + cookies.length);
-    });
-}
-  
-// Ao instalar ou atualizar a extensão, colete todos os cookies
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Extensão instalada. Coletando cookies...");
-    getAllCookies();
-});
+const verCookie = window.document.getElementById('verCookie')
+const delCookie = window.document.getElementById('deconstarCookie')
+const lista = window.document.getElementById('lista')
+const cookies = window.document.cookie
 
-// Escuta eventos de mudança de cookies (criação, alteração, remoção)
-chrome.cookies.onChanged.addListener(function(changeInfo) {
-    console.log('Mudança de Cookie detectada:');
-    console.log(changeInfo);
-});
+let controle = true
+
+verCookie.addEventListener('mousedown', listar)
+delCookie.addEventListener('mousedown', deletar)
+
+function deletar() {
+    // criar uma funcao para deletar o cookies
+}
+
+function listar() { // mostra todos os cookies salvos na pagina
+    obtendoCookies()
+}
+
+function obtendoCookies() {
+    const cookieArray = cookies.split('; ')//constante para separar os cookies em pares nome=valor
+
+    let cookieStr = '<ul>' // converte o array em um objeto mais legivel
+    cookieArray.forEach(cookies => {
+        const [name, value] = cookies.split('=')
+        cookieStr += `<li><strong>${decodeURIComponent(name)}:</strong>${decodeURIComponent(value)}</li>`
+    })
+    return cookieStr
+}
+
+// incrementar uma API para coletar todos os cookies do navegador não só o site atual
+//mudar os arquivos pois para criar uma extenção tem um modo padrao de se colocar os arquivos e pastas
+
+// 0, ...
