f9f547cd3b58d4084555a30f7bebcfe739307214
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2024-10-22 11:16:12 -0300

    Delete background.js

--- Chrome-extensão/background.js
+++
@@ -1,7 +0,0 @@
-chrome.tabs.onUpdated.addListener((changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url) {
-        chrome.cookies.getAll({ url: tab.url }, (cookies) => {
-            chrome.storage.local.set({ [tab.url]: cookies });
-        });
-    }
-  });
